86d648f0926542425e0411ad927b991af545506d
Ulisse Mini <ulisse.mini@gmail.com>
2021-03-08 14:25:03 -0500

    Massive speedup by only grabbing cookies

--- flowdash-khan.py
+++ flowdash-khan.py
@@ -3,6 +3,8 @@ import time
 import os
 import os.path
 from glob import glob
+import shutil
+import sqlite3
 
 import requests
 from requests.auth import HTTPBasicAuth
@@ -10,10 +12,37 @@ from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 load_dotenv()
 
-
 auth = HTTPBasicAuth(os.environ['FLOWDASH_USER'], os.environ['FLOWDASH_API_PASS'])
 
-def get_masterypoints() -> int:
+def get_cookies(where="HOST='www.khanacademy.org'") -> list:
+    "Get firefox cookies"
+
+    firefox_profile_path = glob(os.path.join(os.environ['HOME'], '.mozilla/firefox/*.default-release'))[0]
+
+    src = os.path.join(firefox_profile_path, 'cookies.sqlite')
+    dst = '/tmp/cookies.sqlite'
+
+    # stupid hack since database is locked when in use, and I can't
+    # figure out how to bypass it, or read the DB into memory
+    shutil.copyfile(src, dst)
+
+    with sqlite3.connect(dst) as conn:
+        cursor = conn.execute(f'SELECT * FROM moz_cookies WHERE {where}')
+        cookies_data = cursor.fetchall()
+        keys = [d[0] for d in cursor.description]
+
+        # TODO: Explicitly do this, some names arent the same
+        # (host in sql vs domain in selenium), but it should be fine as long as
+        # value and name are the same.
+        cookies = [dict(zip(keys, cookie)) for cookie in cookies_data]
+
+    # remove the temp database
+    os.remove(dst)
+
+    return cookies
+
+
+def get_masterypoints(headless: bool = True) -> int:
     "Get the mastery points from khan academy using selenium"
 
     from selenium import webdriver
@@ -22,11 +51,18 @@ def get_masterypoints() -> int:
     url = 'https://www.khanacademy.org/math/multivariable-calculus'
     selector = '[data-test-id="subject-points-earned-label"]'
 
-    firefox_profile_path = glob(os.path.join(os.environ['HOME'], '.mozilla/firefox/*.default-release'))[0]
-    print('Loading profile from ' + firefox_profile_path)
-    fp = webdriver.FirefoxProfile(firefox_profile_path)
-    print('Launching Firefox webdriver with profile')
-    driver = webdriver.Firefox(fp)
+    print('Launching webdriver')
+    options = webdriver.FirefoxOptions()
+    options.headless = headless
+    driver = webdriver.Firefox(options=options)
+
+    print('Adding cookies')
+
+    # This is cursed https://stackoverflow.com/a/28331099
+    driver.get('https://www.khanacademy.org/robots.txt')
+    cookies = get_cookies()
+    for cookie in cookies:
+        driver.add_cookie(cookie)
 
     print('Navigating to ' + url)
     driver.get(url)
@@ -53,6 +89,7 @@ def track_masterypoints(mastery_points: int):
 
     url = 'https://flowdash.co/api/tracking'
 
+    print(f'Sending {mastery_points} to flowdash')
     resp = requests.post(url, auth=auth, data={
         "date": time.strftime('%Y-%m-%d'),
         "data": json.dumps({
@@ -66,8 +103,8 @@ def track_masterypoints(mastery_points: int):
 
 def main():
     "Log mastery points to flowdash"
-    # very slow, starting firefox takes a long time
     mastery_points = get_masterypoints()
+    print(mastery_points)
     track_masterypoints(mastery_points)
 
 if __name__ == '__main__':
